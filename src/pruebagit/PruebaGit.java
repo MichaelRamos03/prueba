00e944e0f15b4355098083cba58e5b8644023576
crr99 <crr99@Ramos>
2024-09-16 19:44:54 -0600

    Ejemplo uso de git a netbeans

---
+++ src/pruebagit/PruebaGit.java
@@ -0,0 +1,16 @@
+
+package pruebagit;
+
+/*
+ @author Michael Ramos
+*/
+public class PruebaGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
